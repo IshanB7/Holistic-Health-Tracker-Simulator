5960d5105d9f9efb9e241380509f52b803839e8a
Ishan Bhalla <ishanb1569@gmail.com>
2024-11-20 10:45:12 -0500

    updated view

--- Project/ui/mainwindow.cpp
+++ Project/ui/mainwindow.cpp
@@ -4,13 +4,20 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow),
+    app(App())
 {
     ui->setupUi(this);
-    App();
+    ui->tabWidget->setCurrentIndex(0);
+    connect(ui->tabWidget, SIGNAL (currentChanged(int)), this, SLOT (reloadTab(int)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::reloadTab(int i) {
+    if (i == 1) ui->profiles->reload();
+    // else if (i == 2) ui->history->reload();
 }
