c1897a03c274c0bb2c51f7bbc18a2bef45f38db5
Ishan Bhalla <ishanb1569@gmail.com>
2024-11-20 10:43:31 -0500

    updates

--- Project/App.cpp
+++ Project/App.cpp
@@ -1,5 +1,6 @@
 #include "App.h"
 #include "Radotech.h"
+#include <iostream>
 
 Profile* App::profiles[5] = {nullptr}; 
 int App::currentProfile = 0;
@@ -18,3 +19,14 @@ App::~App() {
 }
 
 Profile* App::user() { return profiles[currentProfile]; }
+
+std::vector<Profile *> App::users() {
+    std::vector<Profile *> returnVector;
+    for (int i = 0; i < 5; ++i) {
+        if (profiles[i] != nullptr) {
+            returnVector.push_back(profiles[i]);
+        }
+    }
+
+    return returnVector;
+}
--- Project/App.h
+++ Project/App.h
@@ -2,12 +2,14 @@
 #define APP_H
 
 #include "Profile.h"
+#include <vector>
 
 class App {
 
 public:
     App();
     ~App();
+    static std::vector<Profile*> users();
     static Profile* user();
     static void setCurrentProfile(int);
 
